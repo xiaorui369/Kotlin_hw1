2924042357dc19bec07ced8989fcb9e8db8c15c4
xiaorui369 <60142672+xiaorui369@users.noreply.github.com>
2020-01-24 18:43:30 +0300

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,24 +1,24 @@
-fun calculateFee(total: Int, amount: Int, exclusive: Boolean = false ): Double{
-    val fee = amount.toDouble()
-    val total = total
+fun calculateFee(total: Int, amount: Int, exclusive: Boolean = false ): Int{
+    amount.toDouble()
     var tax:Double = 0.0
-
-    if (total in 1..1000){
-        tax = 0.3
-    }
-    else if(total in 1001..10_000){
-        tax = 0.25
-    }
-    else if (total in 10_001..50_000){
-        tax = 0.2
-    }
-    else if (total > 50_001){
-        tax = 0.1
+    when {
+        total in 1..1000 -> {
+            tax = 0.3
+        }
+        total in 1001..10_000 -> {
+            tax = 0.25
+        }
+        total in 10_001..50_000 -> {
+            tax = 0.2
+        }
+        total > 50_001 -> {
+            tax = 0.1
+        }
     }
     if (exclusive){
         tax -= 0.05
     }
-    return amount.toDouble() - (fee - (fee * tax))
+    return (amount.toDouble() * tax).toInt()
 }
 
 
